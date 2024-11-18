b22c9fc0b26cacc6c0781cd3ccf6ddd645885c23
Vatsal Nayi <vatsalnayi69@gmail.com>
2024-11-18 19:52:26 +0530

    first commit

---
+++ lib/pages/my_game.dart
@@ -0,0 +1,33 @@
+import 'dart:async';
+import 'dart:ui';
+import 'package:flame/components.dart';
+import 'package:flame/events.dart';
+import 'package:flame/game.dart';
+// import 'package:flame/input.dart';
+
+class MyGame extends FlameGame with TapDetector {
+  late SpriteComponent player;
+
+  @override
+  FutureOr<void> onLoad() async {
+    player = SpriteComponent(
+      sprite: await loadSprite('player.jpeg'),
+      size: Vector2(50, 50),
+      position: Vector2(100, 400),
+    );
+    add(player);
+  }
+
+  @override
+  void onTapDown(TapDownInfo info) {
+    player.y -= 50;
+    // super.onTapDown(info);
+  }
+
+  @override
+  void update(double dt) {
+    super.update(dt);
+    player.y += 1; // Gravity effect
+    if (player.y > 400) player.y = 400; // prevent falling off-screen
+  }
+}
